bfa6119e9a78ead0806873fe65c209401cdad2b0
Alessio Olivieri <alessio.olivieri@enginius.com>
2023-11-29 10:23:14 +0100

    1st commit

---
+++ fmc-java-be/src/main/java/it/olly/fmcbe/FmcJavaBeApplication.java
@@ -0,0 +1,13 @@
+package it.olly.fmcbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmcJavaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmcJavaBeApplication.class, args);
+	}
+
+}
---
+++ fmc-java-be/src/test/java/it/olly/fmcbe/FmcJavaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package it.olly.fmcbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmcJavaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
