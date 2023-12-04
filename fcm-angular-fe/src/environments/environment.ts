9af737da70d54e98f0f8250dd8db320db959bb47
Alessio Olivieri <alessio.olivieri@enginius.com>
2023-12-04 11:08:34 +0100

    base config

---
+++ fcm-angular-fe/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "config data from firebase console",
+        authDomain: "config data from firebase console",
+        databaseURL: "",
+        projectId: "config data from firebase console",
+        storageBucket: "config data from firebase console",
+        messagingSenderId: "config data from firebase console",
+        appId: "config data from firebase console",
+        measurementId: "",
+        vapidKey: "config data from firebase console"
+      }
+  };
